f3890255796070f778e12b4ff873d7116078d031
Afifah Ilyana <AfifahIlyana01@gmail.com>
2018-10-08 14:19:26 +0800

    Score

---
+++ Survival/Assets/Scripts/Diamond.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour {
+    
+    // When enter trigger
+    // Call Collect function from player
+    // Destroy gameobject
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.tag == "Player") {
+            DiamondCollect diamondCollect = other.gameObject.GetComponent<DiamondCollect>();
+            diamondCollect.CollectDiamond();
+
+            Destroy(gameObject, 0f);
+
+        }
+    }
+
+}
