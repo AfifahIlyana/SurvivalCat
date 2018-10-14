cfb49e0d2b02d2c73bcbbe46dc085e015c53248f
Omar <umar_317@hotmail.com>
2018-10-14 13:32:08 +0800

    rotation logics v2

--- Survival/Assets/_Test/Scripts/PlayerMovement.cs
+++ Survival/Assets/_Test/Scripts/PlayerMovement.cs
@@ -4,39 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour 
 {
-    public bool m_isRotating = false;
+    public bool[] m_isRotating = {false, false};
     float m_initialRotation = 0;
 
     public void RotatePlayer()
     {
-        if (Input.GetKeyDown(KeyCode.Q) || m_isRotating)
+        if (Input.GetKeyDown(KeyCode.Q) || m_isRotating[1])
         {
-            Rotate(1f);
+            Rotate( 1f, 1);
         }
-        else if (Input.GetKeyDown(KeyCode.E) || m_isRotating)
+        else if (Input.GetKeyDown(KeyCode.E) || m_isRotating[0])
         {
-            Rotate(-1f);
+            Rotate(-1f, 0);
         }
     }
 
-    public void Rotate(float dir)
+    private void Rotate(float dir, int i)
     {
-        if (!m_isRotating)
+        if (!m_isRotating[i])
         {
-            m_isRotating = true;
+            m_isRotating[i] = true;
             m_initialRotation = transform.rotation.eulerAngles.y;
-            Debug.Log("initial " + m_initialRotation);
         }
 
-        float currentAngle = transform.rotation.eulerAngles.y;
-        transform.rotation = Quaternion.AngleAxis(currentAngle + (Time.deltaTime * 90f * dir), Vector3.up);
+        transform.Rotate(Vector3.up, 90f * dir * Time.deltaTime, Space.World);
 
-        if ( Mathf.Abs(m_initialRotation - transform.rotation.eulerAngles.y) > 90f)
+        if (Mathf.Abs(m_initialRotation - transform.rotation.eulerAngles.y) >= 90f)
         {
-            m_isRotating = false;
+            m_isRotating[i] = false;
             m_initialRotation += 90f * dir;
-            transform.rotation = Quaternion.AngleAxis(m_initialRotation, Vector3.up);
-            Debug.Log("Final " + m_initialRotation);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, m_initialRotation, transform.eulerAngles.z);
         }
     }
+
+    //Rotation alternative
+    //float currentAngle = transform.rotation.eulerAngles.y;
+    //transform.rotation = Quaternion.AngleAxis(currentAngle + (Time.deltaTime * -90f), Vector3.up);
+    //
+    //Quaternion target = Quaternion.Euler(0, 90 * dir, 0);
+    //// Dampen towards the target rotation
+    //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 5f);
 }
