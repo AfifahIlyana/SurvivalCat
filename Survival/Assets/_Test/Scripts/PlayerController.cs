3771cce0bef670acc7c19a06a563035be6336584
Omar <umar_317@hotmail.com>
2018-10-14 11:13:07 +0800

    Add player and camera rotation logic.

--- Survival/Assets/_Test/Scripts/PlayerController.cs
+++ Survival/Assets/_Test/Scripts/PlayerController.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour 
+{
+    PlayerMovement m_playerMovement;
+    Animator m_animator;
+
     public float maxSpeed = 10f;
     bool facingRight = true;
-
-    Animator anim;
 
     bool grounded = false;
     public Transform groundCheck;
@@ -16,11 +20,14 @@ public class PlayerController : MonoBehaviour {
 
     public float jumpForce = 700f;
 
-	void Start () {
-        anim = GetComponent<Animator>();
+	void Start () 
+    {
+        m_playerMovement = GetComponent<PlayerMovement>();
+        m_animator = GetComponent<Animator>();
 	}
 
-    void FixedUpdate() {
+    void FixedUpdate() 
+    {
 
         Rigidbody rb = GetComponent<Rigidbody>();
 
@@ -31,7 +38,7 @@ public class PlayerController : MonoBehaviour {
 
         float move = Input.GetAxis("Horizontal");
 
-        anim.SetFloat("speed", Mathf.Abs(move));
+        m_animator.SetFloat("speed", Mathf.Abs(move));
         
         rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
 
@@ -40,13 +47,19 @@ public class PlayerController : MonoBehaviour {
         else if (move < 0 && facingRight)
             Flip();
         
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump")) 
+        {
             //anim.SetBool("ground", false);
             rb.AddForce(new Vector2(move, jumpForce));
         }
 
     }
-    
+
+    private void Update()
+    {
+        m_playerMovement.RotatePlayer();
+    }
+
     void Flip() {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
