123afcb93e91443ea459538032819801fc839627
LordSayur <umar_317@hotmail.com>
2018-10-14 21:14:58 +0800

    Change environment script to rotate with camera. add script to environment and camera.

--- Survival/Assets/RotateWithCamera.cs
+++ Survival/Assets/RotateWithCamera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Environment : MonoBehaviour 
+public class RotateWithCamera : MonoBehaviour 
 {
 
     public Transform m_player;
