384247755091f456733e72bcc7562b3798609dde
Afifah Ilyana <AfifahIlyana01@gmail.com>
2018-10-15 11:26:41 +0800

    Button

---
+++ Survival/Assets/PlayerControllerPlaceholder.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerPlaceholder : MonoBehaviour {
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+    Animator anim;
+
+    //bool grounded = false;
+    //public Transform groundCheck;
+    //float groundRadius = 0.2f;
+    //public LayerMask whatIsGround;
+
+    public float jumpForce = 700f;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    void FixedUpdate() {
+
+        Rigidbody rb2d = GetComponent<Rigidbody>();
+
+        //grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        //anim.SetBool("ground", grounded);
+
+        anim.SetFloat("vSpeed", rb2d.velocity.y);
+
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", Mathf.Abs(move));
+        
+        rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+    }
+
+    void Update() {
+        Rigidbody rb2d = GetComponent<Rigidbody>();
+
+        if (Input.GetButtonDown("Jump")) {
+            //anim.SetBool("ground", false);
+            rb2d.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    
+}
---
+++ Survival/Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    private Animator animButton;
+
+    private void Start() {
+        animButton = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Player") {
+
+            animButton.SetBool("ButtonDown", true);
+
+            // Disable character control
+            // Trigger tower rotation
+            // Enable character control
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        animButton.SetBool("ButtonDown", false);
+    }
+    
+
+}
