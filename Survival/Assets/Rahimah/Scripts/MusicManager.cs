96e002ce3914a6dba1ab02eca407fd81a79e705e
RAHIMAHAZ\Owner <raa.eimah@gmail.com>
2018-10-18 16:11:23 +0800

    personal folder rahimah

---
+++ Survival/Assets/Rahimah/Scripts/MusicManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+      private MusicPlayer music;
+      public Button musicToggleButton;
+      public Sprite [] offOnbutton;
+
+    void Start()
+    {
+        music = GameObject.FindObjectOfType<MusicPlayer>();
+        UpdateIconAndVolume();
+    } 
+
+    public void PauseMusic()
+    {
+        music.ToggleSound();
+        UpdateIconAndVolume();
+    }
+
+    void UpdateIconAndVolume()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            musicToggleButton.GetComponent<Image>().sprite = offOnbutton[0];
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            musicToggleButton.GetComponent<Image>().sprite = offOnbutton[1];
+        }
+    }
+
+     
+}
+
+
+
---
+++ Survival/Assets/Rahimah/Scripts/SpawnCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnCharacter : MonoBehaviour {
+
+    public GameObject[] Characters;
+    public Transform PlayerSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(Characters[PlayerNum.CharacterNum],PlayerSpawnPoint.position, PlayerSpawnPoint.rotation);
+    }
+	
+
+}
