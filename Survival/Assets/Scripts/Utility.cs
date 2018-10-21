5426bbe8c2faeb6a31e0b42aa9c010cf1aec8855
LordSayur <umar_317@hotmail.com>
2018-10-21 13:43:29 +0800

    Add Utility class. move get axis function from character controller to utility class and add static attribute

---
+++ Survival/Assets/Scripts/Utility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour 
+{
+    public static Vector3 GetAxis()
+    {
+        return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+}
