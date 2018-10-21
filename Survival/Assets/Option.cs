2645f38ccbc3541f53c879d1a530a0f4cca5b559
Afifah Ilyana <AfifahIlyana01@gmail.com>
2018-10-21 13:51:29 +0800

    MainMenu

---
+++ Survival/Assets/Option.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour {
+
+    public GameObject optionPanel;
+
+    private bool isShown = false;
+
+    public void ShowOptions() {
+
+        if (isShown == false) {
+            isShown = true;
+            optionPanel.SetActive(true);
+
+        } else {
+            isShown = false;
+            optionPanel.SetActive(false);
+
+        }
+    }
+
+}
