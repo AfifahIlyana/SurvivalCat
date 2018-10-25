6f63133c0580029e5fcebfd38ce5e16af06ea93f
Afifah Ilyana <AfifahIlyana01@gmail.com>
2018-10-25 15:49:19 +0800

    Pause

--- Survival/Assets/Scripts/GameOver.cs
+++ Survival/Assets/Scripts/GameOver.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour {
 
     public GameObject gameOverScreen; // add the game over screen UI 
     public SaveBestScore scoreSave;
 
+    private Text scoreText;
+
     public void TriggerGameOver() {
         
         gameOverScreen.SetActive(true);
         scoreSave.GetFinalScore(); // Save score if higher than the current best score
+
+        int finalScore = int.Parse(scoreSave.score.text);
+        scoreText = GameObject.FindGameObjectWithTag("FinalScore").GetComponent<Text>();
+        scoreText.text = finalScore.ToString();
+
+        scoreText = GameObject.FindGameObjectWithTag("BestScore").GetComponent<Text>();
+        scoreText.text = PlayerPrefs.GetInt("Best").ToString();
 
         // Add some sound effect here
 
