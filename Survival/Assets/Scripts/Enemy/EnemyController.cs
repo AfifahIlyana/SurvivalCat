048577de8c3c404d8270d505dc943ff72c4ee90f
Bazilah <>
2018-11-05 09:17:59 +0800

    -

--- Survival/Assets/Scripts/Enemy/EnemyController.cs
+++ Survival/Assets/Scripts/Enemy/EnemyController.cs
@@ -7,17 +7,22 @@
 [RequireComponent(typeof(Rigidbody))]
 public class EnemyController : MonoBehaviour 
 {
+    private Animator m_animator;
     private Rigidbody m_rigidBody;
-    private MyUIManager m_myUiManager;
+
     private EnemyAttack m_enemyAttack;
     private EnemyMovement m_enemyMovement;
 
     private float m_timeSinceLastDamage;
 
+
+    [SerializeField]
+    private float m_attackCoolDown = 1f;
+
     private void Start()
     {
+        m_animator = GetComponent<Animator>();
         m_rigidBody = GetComponent<Rigidbody>();
-        m_myUiManager = GameObject.Find("Canvas").GetComponent<MyUIManager>();
         m_enemyAttack = GetComponent<EnemyAttack>();
         m_enemyMovement = GetComponent<EnemyMovement>();
     }
@@ -37,22 +42,22 @@ public class EnemyController : MonoBehaviour
         m_enemyMovement.CheckEdgesPlatform(other);
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            if (m_timeSinceLastDamage > 1f)
-            {
-                other.GetComponent<PlayerHealth>().TakeDamage(1, other.gameObject);
-                m_myUiManager.UpdateHealthStatus(other.gameObject.GetComponent<PlayerData>().m_health, false);
-
-                m_timeSinceLastDamage = 0;
-            }
-        }
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        m_enemyAttack.Attack(collision);
+        if (m_timeSinceLastDamage > m_attackCoolDown)
+        {
+            m_animator.SetBool("isAttacking", true);
+            m_enemyAttack.Attack(collision);
+            m_timeSinceLastDamage = 0;
+        }
+
+        else
+        {
+              m_animator.SetBool("isAttacking", true);
+        }
+
     }
+
+
 }
--- Survival/Assets/Scripts/Player/PlayerHealth.cs
+++ Survival/Assets/Scripts/Player/PlayerHealth.cs
@@ -5,6 +5,12 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage (int reduceHealth, GameObject player)
     {
         player.GetComponent<PlayerData>().m_health -= reduceHealth;
+
+        if (player.GetComponent<PlayerData>().m_health <= 0)
+        {
+            GameOver gameover = GameObject.FindGameObjectWithTag("GameOverTrigger").GetComponent<GameOver>();
+            gameover.TriggerGameOver();
+        }
     }
 
     public void AddHealth (int addHealth, GameObject player)
