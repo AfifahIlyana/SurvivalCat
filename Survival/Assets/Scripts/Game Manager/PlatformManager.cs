437e1740cbf0025c3caa78f55ae7230bb6948d4b
Afifah Ilyana <AfifahIlyana01@gmail.com>
2018-11-05 13:43:26 +0800

    Add

---
+++ Survival/Assets/Scripts/Game Manager/PlatformManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour 
+{
+    private GameObject m_player;
+    private PlatformSpawner m_platformSpawner;
+
+    [SerializeField]
+    private int platformHolderNumber;
+
+    void Start () 
+    {
+        m_player = GameObject.FindWithTag("Player");
+        m_platformSpawner = GetComponent<PlatformSpawner>();
+	}
+	
+	void Update () 
+    {
+        if (Utility.objectMovedUp == gameObject.transform.name)
+        {
+            if (m_platformSpawner != null)
+            {
+                m_platformSpawner.ReshufflePlatformPosition(platformHolderNumber);
+            }
+        }
+    }
+}
