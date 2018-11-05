cbaf93d24cadee9ae9e873f543ef9c556b6d0067
RAHIMAHAZ\Owner <raa.eimah@gmail.com>
2018-11-05 09:36:59 +0800

    Mute (music) scripts

---
+++ Survival/Assets/_Rahimah/Scripts/SFXManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour {
+
+    public Image sfxToggleButton;
+    public Sprite[] offOnbutton;
+    private SFX sound;
+
+    void Start()
+    {
+        sound = GameObject.FindObjectOfType<SFX>();
+        UpdateIconAndVolumes();
+    }
+
+    public void Pausesfx()
+    {
+        sound.ToggleSoundfx();
+        UpdateIconAndVolumes();
+    }
+
+    void UpdateIconAndVolumes()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            sound.GetComponent<AudioSource>().volume = 1;
+            sfxToggleButton.GetComponent<Image>().sprite = offOnbutton[0];
+        }
+        else
+        {
+            sound.GetComponent<AudioSource>().volume = 0;
+            sfxToggleButton.GetComponent<Image>().sprite = offOnbutton[1];
+        }
+    }
+}
