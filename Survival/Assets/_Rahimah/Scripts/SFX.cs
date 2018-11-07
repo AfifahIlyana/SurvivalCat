a53e6291069dc8f638b6a4be74c17a29994ac0cb
RAHIMAHAZ\Owner <raa.eimah@gmail.com>
2018-11-07 10:52:31 +0800

    si monkey sprite & prefab

--- Survival/Assets/_Rahimah/Scripts/SFX.cs
+++ Survival/Assets/_Rahimah/Scripts/SFX.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class SFX : MonoBehaviour {
 
-   
-    public void ToggleSoundfx()
-    {
-        if (PlayerPrefs.GetInt("Muted", 0) == 0)
-        {
-            PlayerPrefs.SetInt("Muted", 1);
-        }
-        else
-        {
-            PlayerPrefs.SetInt("Muted", 0);
-        }
-    }
+
+  
 }
