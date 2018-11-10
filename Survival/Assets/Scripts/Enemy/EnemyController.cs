e77fb2834ce0b49a6bc8ffa7564b40063cd2f9f2
Afifah Ilyana <AfifahIlyana01@gmail.com>
2018-11-10 16:34:33 +0800

    Scene health and score fix

--- Survival/Assets/Scripts/Enemy/EnemyController.cs
+++ Survival/Assets/Scripts/Enemy/EnemyController.cs
@@ -25,7 +25,7 @@ public class EnemyController : MonoBehaviour
         m_enemyAttack = GetComponent<EnemyAttack>();
         m_enemyMovement = GetComponent<EnemyMovement>();
 
-        m_playerHealth = GetComponent<PlayerHealth>();
+        m_playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
         m_myUiManager = GameObject.Find("Canvas").GetComponent<MyUIManager>();
     }
 
@@ -45,15 +45,18 @@ public class EnemyController : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
         if (m_timeSinceLastDamage > 1f)
         {
-            m_animator.SetBool("isAttacking", true);
-            EnemyMovement.currentSpeed = 0;
+            
 
             if (collision.transform.tag == "Player")
             {
+
+                m_animator.SetBool("isAttacking", true);
+                EnemyMovement.currentSpeed = 0;
+
                 Debug.Log("inda mau kurang health si " + collision.gameObject);
 
                 m_playerHealth.TakeDamage(1, collision.gameObject);
