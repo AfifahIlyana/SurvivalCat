da5c2dabfdb86e95eab48cdcfdc6984a15b88820
Bazilah <>
2018-11-17 10:09:19 +0800

    ObjectSpawner : fix error (mau sudah spawning the Object)

--- Survival/Assets/Scripts/Spawner/ObjectSpawner.cs
+++ Survival/Assets/Scripts/Spawner/ObjectSpawner.cs
@@ -43,7 +43,7 @@ public class ObjectSpawner : MonoBehaviour {
 
                 foreach(Transform child in transform)
                 {
-                    GameObject enemy = Instantiate(enemyPrefab[i], transform.position, transform.rotation);
+                    GameObject enemy = Instantiate(enemyPrefab[i], transform.position, Quaternion.identity);
                     enemy.transform.parent = child;
                    // Debug.Log("Enemy: " + enemyPrefab[i].name);
                 }
