033bc930f2e9c3a53f71037f4533e68e8f94fc8e
RAHIMAHAZ\Owner <raa.eimah@gmail.com>
2018-11-19 14:59:01 +0800

    sound manager (Music & SFX) :')

---
+++ Survival/Assets/_Rahimah/Scripts/SFX_icon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SFX_icon : MonoBehaviour {
+
+    public Image sfxToggleButton;
+    public Sprite[] offOnbutton;
+
+    public void OnToggle()
+    {
+        sfxToggleButton.GetComponent<Image>().sprite = offOnbutton[0];
+    }
+
+    public void OffToggle()
+    {
+        sfxToggleButton.GetComponent<Image>().sprite = offOnbutton[1];
+    }
+
+}
