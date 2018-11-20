9131754c5ef9fa281e00d02d25f5b95081642fbb
Afifah Ilyana <AfifahIlyana01@gmail.com>
2018-11-20 12:30:10 +0800

    Character lock

--- Survival/Assets/Scripts/_/BestScoreGet.cs
+++ Survival/Assets/Scripts/_/BestScoreGet.cs
@@ -10,9 +10,22 @@ public class BestScoreGet : MonoBehaviour {
     private void Start() {
 
         bestScoreText = GetComponent<Text>();
-
         bestScoreText.text = PlayerPrefs.GetInt("Best").ToString();
 
+        CheckPlayable();
+
+    }
+
+    private void CheckPlayable() {
+        int score = PlayerPrefs.GetInt("Best");
+        Debug.Log(score);
+        if (score >= 1000 && score < 5000) {
+            PlayerPrefs.SetInt("Playable", 1);
+            Debug.Log("checkign");
+        } else if (score >= 5000) {
+            PlayerPrefs.SetInt("Playable", 2);
+
+        }
 
     }
 
---
+++ Survival/Assets/UnlockCharacter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockCharacter : MonoBehaviour {
+    
+    public GameObject dogLock;
+    public GameObject monkeyLock;
+
+    public GameObject dogUnlock;
+    public GameObject monkeyUnlock;
+
+    private int playableCharacter;
+    
+    private void Start() {
+        
+        playableCharacter = PlayerPrefs.GetInt("Playable", 0);
+
+        Debug.Log(playableCharacter);
+
+        if (playableCharacter == 1 || playableCharacter == 2) {
+            dogLock.SetActive(false);
+            dogUnlock.SetActive(true);
+            
+            if (playableCharacter == 2) {
+                dogUnlock.SetActive(false);
+                monkeyUnlock.SetActive(true);
+                
+            }
+
+        }
+    }
+
+
+}
